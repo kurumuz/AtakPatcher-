b2c4420323ffc31c63d1eff3c63e02a03b120c8e
kurumuz <kurumuz1@gmail.com>
2018-03-16 18:13:48 +0300

    Completed the code

--- AtakPatcher/AtakPatcher/Form1.Designer.cs
+++ AtakPatcher/AtakPatcher/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -39,13 +40,17 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(311, 79);
+            this.button1.Location = new System.Drawing.Point(54, 27);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 33);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
             // 
             // Form1
             // 
@@ -63,6 +68,7 @@
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
     }
 }
 
--- AtakPatcher/AtakPatcher/Form1.cs
+++ AtakPatcher/AtakPatcher/Form1.cs
@@ -13,6 +13,12 @@ namespace AtakPatcher
 {
     public partial class Form1 : Form
     {
+        static void satirDegistir(string newText, string fileName, int line_to_edit)
+        {
+            string[] arrLine = File.ReadAllLines(fileName);
+            arrLine[line_to_edit - 1] = newText;
+            File.WriteAllLines(fileName, arrLine);
+        }
         public Form1()
         {
             InitializeComponent();
@@ -20,20 +26,44 @@ namespace AtakPatcher
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
-
+            //8.satırda şifrelenen seri no tutuluyor o değiştirilmeli.
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog dosya = new OpenFileDialog();
-            dosya.Filter = "Atak USB KEY dosyası |*.veri";
-            dosya.Title = "Patchlenecek keyi seçin.";
-            dosya.ShowDialog();
-            if (dosya.ShowDialog() == DialogResult.OK)
+            int x = 1;
+            MessageBox.Show("Patchlemek istediğiniz atak.veri dosyasını seçiniz(USB kökünde gizli olarak bulunur.)");
+            OpenFileDialog hedef = new OpenFileDialog();
+
+
+            hedef.Filter = "Atak USB KEY dosyası |*.veri";
+            hedef.Title = "Patchlenecek atak.veri'yi seçiniz.";
+            if (hedef.ShowDialog() == DialogResult.OK)
             {
-                string dosyaYolu = dosya.FileName;
+                x++;
+            }
+            MessageBox.Show("Kendi ürettiğiniz atak.veri dosyasını seçiniz(Atak 1.09 sürümünden üretebilirsiniz.)");
+            OpenFileDialog kendidosyasi = new OpenFileDialog();
+            kendidosyasi.Filter = "Atak USB KEY dosyası |*.veri";
+            hedef.Title = "Kendi atak.verinizi seçiniz.";
+
+            if (kendidosyasi.ShowDialog() == DialogResult.OK & x == 2)
+            {
+                string anapath = hedef.FileName;
+                string kendipath = kendidosyasi.FileName;
+                MessageBox.Show(anapath + "     " + kendipath);
+                x = 1;
+                string usbserino = File.ReadLines(kendipath).Skip(7).Take(1).First();
+                MessageBox.Show("Dosyalar seçildi, patchlenme işlemi yapılıyor.");
+                satirDegistir(usbserino, anapath, 8);
+                MessageBox.Show("Patchlenme işlemi tamamnlandı.");
+            }
+            else { MessageBox.Show("Her iki dosyayıda seçmelisiniz.");
+            }
+            
+
 
             }
         }
     }
-}
+
